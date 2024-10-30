b29f3963d34e714b961922661d88c44b5e3c10d7
mamoona-batool <csreplyllc@gmail.com>
2024-10-30 14:44:19 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        alert('Your message has been sent!');
+});
+});
